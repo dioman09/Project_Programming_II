92a57d7913cf54bc67309aa31005110dc1f603a5
Dioman Anaya <anayapayaresdioman@gmail.com>
2025-06-03 19:06:30 -0500

    Subiendo proyecto, ya estan creados los archivos necesarios para empezar a configurar la vista del Login(View_Login.fxml)

---
+++ src/Aplication/Aplication.java
@@ -0,0 +1,33 @@
+package Aplication;
+
+import java.io.IOException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Aplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/Views/View_Login.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);            
+            stage.show();
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
+
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch();
+    }
+}
