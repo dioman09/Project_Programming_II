98aeabfd14c1d6922cd90a431ebed2658e815df4
Dioman Anaya <anayapayaresdioman@gmail.com>
2025-06-05 15:09:55 -0500

    Vista de login para administradores y clientes completada, y conexión con la vista de arranque.

--- src/Controllers/Controller_View_Arranque.java
+++ src/Controllers/Controller_View_Arranque.java
@@ -1,12 +1,21 @@
 package Controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Controller_View_Arranque implements Initializable {
 
@@ -27,14 +36,56 @@ public class Controller_View_Arranque implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        Image imageNormal = new Image(getClass().getResourceAsStream("/Images/Publics/GitHub.png"));
+        Image imageHover = new Image(getClass().getResourceAsStream("/Images/Publics/GitHub2.png"));
+
+        link_01.setOnMouseEntered(event -> ima_link_01.setImage(imageHover));
+        link_01.setOnMouseExited(event -> ima_link_01.setImage(imageNormal));
+
+        Image imageNormal_ = new Image(getClass().getResourceAsStream("/Images/Publics/WhatsApp.png"));
+        Image imageHover_ = new Image(getClass().getResourceAsStream("/Images/Publics/WhatsApp2.png"));
+
+        link_02.setOnMouseEntered(event -> ima_link_02.setImage(imageHover_));
+        link_02.setOnMouseExited(event -> ima_link_02.setImage(imageNormal_));
+    }
+
+    public void run_login(String type) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/View_Login.fxml"));
+            Parent root = loader.load();
+
+            Controller_View_Login controller = loader.getController();
+
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+
+            stage.setScene(scene);
+
+            stage.setOnShown(event -> controller.seeIma(type));
+
+            stage.setOnCloseRequest((WindowEvent value) -> {
+                controller.run_arranque();
+            });
+
+            stage.show();
+
+            Stage miStage = (Stage) this.btn_login.getScene().getWindow();
+            miStage.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Controller_View_Arranque.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
     @FXML
     private void action_event(ActionEvent event) {
+        if (event.getSource() == btn_admin) {
+            run_login("Admin");
+        } else if (event.getSource() == btn_login) {
+            run_login("User");
+        }
     }
 
     @FXML
     private void irLink(ActionEvent event) {
-    }    
+    }
 }
--- src/Controllers/Controller_View_Login.java
+++ src/Controllers/Controller_View_Login.java
@@ -1,14 +1,77 @@
 package Controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
 
 public class Controller_View_Login implements Initializable {
+
+    @FXML
+    private ImageView ima_user;
+    @FXML
+    private Group ima_admin;
+    @FXML
+    private TextField txt_email;
+    @FXML
+    private PasswordField txt_password;
+    @FXML
+    private Button btn_login;
+    @FXML
+    private Hyperlink link_regist;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    public void run_arranque() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/View_Arranque.fxml"));
+            Parent root = loader.load();            
+
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+
+            stage.setScene(scene);           
+            stage.show();
+
+            Stage miStage = (Stage) this.btn_login.getScene().getWindow();
+            miStage.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Controller_View_Login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void seeIma(String type) {
+        if (type.equals("Admin")) {
+            ima_admin.setVisible(true);
+        } else {
+            ima_user.setVisible(true);
+        }
+    }
+
+    @FXML
+    private void eventKey(KeyEvent event) {
+    }
+
+    @FXML
+    private void eventAction(ActionEvent event) {
+    }
+
 }
