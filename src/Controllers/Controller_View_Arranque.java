1586ab04cf27d4b03534b85893e1c33db1c53535
Dioman Anaya <anayapayaresdioman@gmail.com>
2025-06-04 22:20:32 -0500

    Agregando algunas imagenes necesarias y vista de arranque terminada(View_Arranque.fxml).

--- src/Controllers/Controller_View_Arranque.java
+++ src/Controllers/Controller_View_Arranque.java
@@ -2,13 +2,39 @@ package Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 
 public class Controller_View_Arranque implements Initializable {
+
+    @FXML
+    private Button btn_admin;
+    @FXML
+    private Button btn_signing_up;
+    @FXML
+    private Button btn_login;
+    @FXML
+    private Button link_01;
+    @FXML
+    private Button link_02;
+    @FXML
+    private ImageView ima_link_01;
+    @FXML
+    private ImageView ima_link_02;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
+
+    @FXML
+    private void action_event(ActionEvent event) {
+    }
+
+    @FXML
+    private void irLink(ActionEvent event) {
+    }    
 }
